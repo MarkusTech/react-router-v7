3e20393f120d124501745f73c0d747e92a1f2977
MarkusTech <wm.recopelacion@gmail.com>
2025-01-27 11:59:00 +0800

    Update home.tsx

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -3,7 +3,7 @@ import type { Route } from "./+types/home";
 
 export function meta({}: Route.MetaArgs) {
   return [
-    { title: "RESTExplorer" },
+    { title: "WMRExplorer" },
     { name: "Check out country data!", content: "Welcome to RESTExplorer." },
   ];
 }
